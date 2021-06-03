abdaaa2f6307321ecd3a1ceb405d3766757b8339
Nick Cao <nickcao@nichi.co>
2021-06-03 23:00:15 +0800

    test update dns record

--- dnsconfig.js
+++ dnsconfig.js
@@ -2,5 +2,5 @@ var GANDI = NewDnsProvider("gandi", "GANDI_V5");
 var REG_GANDI = NewRegistrar("gandi", "GANDI_V5");
 
 D("scp.link", REG_GANDI, DnsProvider(GANDI),
-    A("test","1.2.3.4")
+    A("test","1.1.1.1")
 );
