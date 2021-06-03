60c2952dc11e0631020c1d9850fe3e9caa55f190
Nick Cao <nickcao@nichi.co>
2021-06-03 23:31:01 +0800

    replace gandi registrar with none to avoid misconfiguration

--- dnsconfig.js
+++ dnsconfig.js
@@ -1,6 +1,6 @@
 var GANDI = NewDnsProvider("gandi", "GANDI_V5");
-var REG_GANDI = NewRegistrar("gandi", "GANDI_V5");
+var REG_NONE = NewRegistrar("none", "NONE");
 
-D("scp.link", REG_GANDI, DnsProvider(GANDI),
+D("scp.link", REG_NONE, DnsProvider(GANDI),
     A("test","1.1.1.1")
 );
