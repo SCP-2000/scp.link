ad7b8e798642f8f87d6a9b3d1f65e9d1852bf4d6
Jerry <isjerryxiao@outlook.com>
2021-06-04 14:25:35 +0800

    register o.scp.link

--- dnsconfig.js
+++ dnsconfig.js
@@ -2,5 +2,10 @@ var GANDI = NewDnsProvider("gandi", "GANDI_V5");
 var REG_NONE = NewRegistrar("none", "NONE");
 
 D("scp.link", REG_NONE, DnsProvider(GANDI),
-    A("test","1.1.1.1")
+    A("test","1.1.1.1"),
+    NS("o", "ns1.he.net."),
+    NS("o", "ns2.he.net."),
+    NS("o", "ns3.he.net."),
+    NS("o", "ns4.he.net."),
+    NS("o", "ns5.he.net.")
 );
